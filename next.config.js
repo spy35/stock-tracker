845f9208554a7cdbb5ff5084b5d30a2e88b99f28
spy35 <spy35@bible.ac.kr>
2025-06-15 07:38:01 +0900

    fix: remove experimental outputFileTracingRoot setting from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  // 파일 경로 추적 문제를 해결하기 위한 설정
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, './'),
-  },
-
   // Next.js 15에서 변경된 설정
   serverExternalPackages: ["@prisma/client", "bcrypt"],
 
